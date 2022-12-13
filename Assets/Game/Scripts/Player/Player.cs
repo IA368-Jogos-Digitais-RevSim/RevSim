52de5dc4b64adff8a54358d06e370db4adb84dc0
Leonardo Rener <leonardo-rener@hotmail.com>
2022-12-13 20:07:57 -0300

    Adicionado audio de passos
    - Feito por Leonardo Passos

--- Assets/Game/Scripts/Player/Player.cs
+++ Assets/Game/Scripts/Player/Player.cs
@@ -17,23 +17,57 @@ public class Player : MonoBehaviour
     private RaycastHit _hit;
     private NavMeshAgent _agent;
     
+    //audio (LeoP)
+    private AudioSource audioSource;
+    private int velox = 0;
+    private bool walking = false;
+
     void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
         UpdateSkin();
+
+        //audio (LeoP)
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
+        {
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _hit, 100))
+            {
                 _agent.destination = _hit.point;
+            }
+        }
 
         var agenteVelocty = _agent?.velocity;
         var velocity = Mathf.Abs(agenteVelocty?.x?? 0f) + Mathf.Abs(agenteVelocty?.z?? 0f);
         _animator.SetFloat(CharacterAnimatorParameters.SPEED, velocity);
+
+        //audio (LeoP)
+        velox = (int) velocity;
+
+        if (velox > 0)
+        {
+            walking = true;
+        }
+        else
+        {
+            walking = false;
+        }
+
+        if (!audioSource.isPlaying && walking)
+        {
+            audioSource.Play();
+        }
+        else if (audioSource.isPlaying && !walking)
+        {
+            audioSource.Stop();
+        }
     }
+
 
     public void UpdateSkin()
     {
